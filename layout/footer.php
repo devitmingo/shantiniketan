2583a518b728de6e3b7af684f019279d35160b04
PatelTarachand <ptltarachand@gmail.com>
2024-05-30 18:40:39 +0530

    snc project

---
+++ layout/footer.php
@@ -0,0 +1,114 @@
+
+        <!-- footer -->
+        <footer class="footer-bg footer-p pt-90" style="background-color: #125875; background-image: url(img/bg/footer-bg.png);">
+            <div class="footer-top pb-70">
+                <div class="container">
+                    <div class="row justify-content-between">
+                        
+                        <div class="col-xl-4 col-lg-4 col-sm-6">
+                            <div class="footer-widget mb-30">
+                                <div class="f-widget-title">
+                                    <h2>About Us</h2>
+                                </div>
+                                <div class="f-contact">
+                                     <p>Suspendisse non sem ante. Cras pretium gravida leo a convallis. Nam malesuada interdum metus, sit amet dictum ante congue eu. Maecenas ut maximus enim.</p>
+                                    
+                                    </div>
+                                 <div class="footer-social mt-10">                                    
+                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="#"><i class="fab fa-instagram"></i></a>
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                </div>    
+                            </div>
+                        </div>
+						<div class="col-xl-4 col-lg-4 col-sm-6">
+                            <div class="footer-widget mb-30">
+                                <div class="f-widget-title">
+                                    <h2>Our Links</h2>
+                                </div>
+                                <div class="footer-link">
+                                    <ul>                                        
+                                        <li><a href="index.php">Home</a></li>
+                                        <li><a href="about.php"> About</a></li>
+                                        <!-- <li><a href="courses.html">Courses</a></li> -->
+                                        <li><a href="contact.php"> Contact Us</a></li>
+                                        <!-- <li><a href="blog.html">Blog </a></li> -->
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                       
+                        <div class="col-xl-4 col-lg-4 col-sm-6">
+                            <div class="footer-widget mb-30">
+                                <div class="f-widget-title">
+                                  <h2>Contact Us</h2>
+                                </div>
+                                <div class="f-contact">
+                                    <ul>
+                                    <li>
+                                        <i class="icon fal fa-phone"></i>
+                                        <span><a href="tel:+917828168418">+91 78281 68418</a><br><a href="tel:+917052101786">+91 88273 76688</a></span>
+                                    </li>
+                                   <li><i class="icon fal fa-envelope"></i>
+                                        <span>
+                                            <a href="mailto:shantiniketan2009@gmail.com">shantiniketan2009@gmail.com</a>
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <i class="icon fal fa-map-marker-check"></i>
+                                        <span>Ring Road No.1, Near Pani Tanki,<br> Changorbhatha, Raipur, Chhattisgarh, 492001 </span>
+                                    </li>
+                                    
+                                </ul>
+                                    
+                                    </div>
+                                
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="copyright-wrap">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4">
+                          <div class="copy-text">
+                                <a href="index.php"><img src="images/snc.jpeg" alt="img" style="height:70px;width:70px;"></a>   
+                            </div>
+                        </div>
+                        <div class="col-lg-4 text-center">          
+                            
+                        </div>
+                       <div class="col-lg-4 text-right text-xl-right">
+                           Copyright &copy; Shanatiniketan College 2023 . All rights reserved. Developed By <a href="https://itmingo.com/" target="_blank" >IT Mingo LLP</a>     
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- footer-end -->
+
+		<!-- JS here -->
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="js/vendor/jquery-3.6.0.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/slick.min.js"></script>
+        <script src="js/ajax-form.js"></script>
+        <script src="js/paroller.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script src="js/js_isotope.pkgd.min.js"></script>
+        <script src="js/imagesloaded.min.js"></script>
+        <script src="js/parallax.min.js"></script>
+        <script src="js/jquery.waypoints.min.js"></script>
+        <script src="js/jquery.counterup.min.js"></script>
+        <script src="js/jquery.scrollUp.min.js"></script>
+        <script src="js/jquery.meanmenu.min.js"></script>
+        <script src="js/parallax-scroll.js"></script>
+        <script src="js/jquery.magnific-popup.min.js"></script>
+        <script src="js/element-in-view.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+
+</html>
